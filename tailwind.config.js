eacb28f4864c831b159bdc71a44e3e1465186b35
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2024-01-24 16:33:18 +0000

    starting header

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-orange": "hsl(26, 100%, 55%)",
+        "primary-pale-orange": "hsl(25, 100 %, 94 %)",
+
+        "neutral-very-dark-blue": "hsl(220, 13%, 13%)",
+        "neutral-Dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "neutral-Grayish-blue": "hsl(220, 14%, 75%)",
+        "neutral-Light-grayish-blue": "hsl(223, 64%, 98%)",
+        "neutral-White": "hsl(0, 0%, 100%)",
+        "neutral-Black": "hsl(0, 0%, 0%)",//(with 75% opacity for lightbox background)
+      },
+      fontSize: {
+        "paragraph": "16px",
+      },
+      fontFamily: {
+        "Kumbh-Sans": ['Kumbh Sans', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
